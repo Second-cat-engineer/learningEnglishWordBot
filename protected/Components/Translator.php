6aa6fa4afb2f4d95802d3209552915e49ca9339d
Safuan Yaylyaev <yaylyaevsafuan@gmail.com>
2020-09-19 08:34:26 +0300

    + объявил тип аргументов и возвращающий тип для метода getTranslate(), также сообщение при возникновение исключения. + объвил возвращаемый тип методу engRusTranslator(). + переименовал имена свойств класса Word.

--- protected/Components/Translator.php
+++ protected/Components/Translator.php
@@ -4,13 +4,12 @@ namespace App\Components;
 
 use App\Exceptions\ErrorTranslator;
 use App\Models\Word;
-use Zanzara\Context;
 
 class Translator
 {
     const BASE_URL = 'https://dictionary.skyeng.ru/api/public/v1/words/search';
 
-    protected static function getTranslate($word)
+    protected static function getTranslate(string $word) : array
     {
         $params['search'] = $word;
         $url = self::BASE_URL . '?' . http_build_query($params);
@@ -21,20 +20,20 @@ class Translator
         );
 
         if (!$response) {
-            throw new ErrorTranslator('Не удалось перевести слово');
+            throw new ErrorTranslator('Не удалось найти перевод данного слова');
         }
         return $response[0];
     }
 
-    public static function engRusTranslator(string $enteredWord)
+    public static function engRusTranslator(string $enteredWord) : Word
     {
         $res = self::getTranslate($enteredWord);
 
         $word = new Word();
         $word->rus_word = $res['meanings'][0]['translation']['text'];
         $word->eng_word = $res['text'];
-        $word->imageUrl = $res['meanings'][0]['imageUrl'];
-        $word->soundUrl = $res['meanings'][0]['soundUrl'];
+        $word->image_url = $res['meanings'][0]['imageUrl'];
+        $word->sound_url = $res['meanings'][0]['soundUrl'];
 
         return $word;
     }
