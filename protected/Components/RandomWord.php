0604c44bd36f4ff2bdd17adfafc313f48e4a7cf5
Safuan Yaylyaev <yaylyaevsafuan@gmail.com>
2020-09-23 21:25:27 +0300

    fixed errors

--- protected/Components/RandomWord.php
+++ protected/Components/RandomWord.php
@@ -6,13 +6,13 @@ use App\Models\Word;
 
 class RandomWord
 {
-    public static function randomWord(int $user_id) : object
+    public static function randomWord(int $user_id)
     {
         $words = Word::findAllByUser($user_id);
-        if (!empty($word)) {
+        if (empty($words)) {
             throw new \Exception('Ваш словарь пуст! Добавьте слова для заучивания');
         }
-        $max = count($words) - 1;
+        $max = count($words)-1;
         $min = 0;
 
         $randomNumber = random_int($min, $max);
